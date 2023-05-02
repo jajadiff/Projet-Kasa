a66be92107df5592c07e5547fe979b3106050a71
Jordan <jordanfurneri57@gmail.com>
2023-05-02 19:30:45 +0200

    Ajout components Footer JS + SCSS + logo

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import footerlogo from "./../assets/logofooter.png"
+
+function Footer() {
+  return <footer className="footer">
+      <img className="footer__logo" src={footerlogo} alt="Kasa, Footer Logo"></img>
+      <p className="footer__txt">© 2020 Kasa. All rights reserved</p>
+  </footer>;
+
+}
+
+export default Footer;
