e0d3a049b9d578e57291fb501239e232bb253288
Jordan <jordanfurneri57@gmail.com>
2023-05-02 08:58:10 +0200

    Ajout components Navbar js

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul className="header__list">
+        <li>
+          <a href="#">ACCUEIL</a>
+        </li>
+
+        <li>
+          <a href="#">A PROPOS</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
