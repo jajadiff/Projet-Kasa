396934351f02b15d2a61127a32dbc9599e4bc983
Jordan <jordanfurneri57@gmail.com>
2023-05-02 19:31:15 +0200

    Ajout des components dans App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-// import logo from './';
+import React from "react";
 import Header from "./components/Header";
+import Banner from "./components/Banner.js";
+import Cartcontainer from "./components/Cartcontainer.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
+      <Cartcontainer />
+      <Footer />
     </>
   );
 }
