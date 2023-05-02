e2a40e7f42c800da71116275bf9e421e1c4de130
Jordan <jordanfurneri57@gmail.com>
2023-05-02 19:28:44 +0200

    Ajout components Banner JS + SCSS

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import banneraccueil from "../assets/banner_accueil.png";
+
+function Banner() {
+  return (
+    <section className="banner">
+      <h2 className="banner__txt">Chez vous, partout et ailleurs</h2>
+    </section>
+  );
+}
+
+export default Banner;
