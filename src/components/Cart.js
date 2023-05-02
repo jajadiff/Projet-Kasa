779fb67b296d2cdebc31df6b8f2b59942578311c
Jordan <jordanfurneri57@gmail.com>
2023-05-02 19:30:19 +0200

    Ajout components Cart JS

---
+++ src/components/Cart.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Cart() {
+  return <article className="container__cart">
+      <p className="container__txt">Titre de la location</p>
+  </article>;
+}
+
+export default Cart;
