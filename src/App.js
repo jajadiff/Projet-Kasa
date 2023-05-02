4fbe1de92134e850c9efe7a899b7bed5694a6611
Jordan <jordanfurneri57@gmail.com>
2023-05-02 08:59:34 +0200

    Ajout components Header dans App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // import logo from './';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
